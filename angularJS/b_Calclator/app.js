0e163c5691a44bb9b350fc02582e2fb1170096c2
Koji <stylethewalker@gmail.com>
2016-12-09 18:14:05 -0200

    dev: angularJS Calculator

--- angularJS/b_Calclator/app.js
+++ angularJS/b_Calclator/app.js
@@ -1,41 +1,106 @@
-
-
-
-
 angular.module('App', [])
 .controller('mainController', ['$scope', function($scope){
 
-  $scope.tasks = [
-    {"name": "Amy quoit", "done": true},
-    {"name": "reascendancy umbrel", "done": true},
-    {"name": "observability fattily", "done": false},
-    {"name": "cartilaginous precounsellor", "done": true},
-    {"name": "substratose faultily", "done": false}
-  ];
-
-  $scope.addNew = function() {
-  	$scope.tasks.push({
-  		"name": $scope.newTaskName,
-  		"done": false
-    });
-  	$scope.newTaskName = "";
+  // functions
+  $scope.go       = _go;
+  $scope.addNum   = _addNum;
+  $scope.addSign  = _addSign;
+  $scope.addPoint = _addPoint;
+  $scope.clearAll = _clearAll;
+  $scope.equal = _equal;
+  state = {
+    "setStandBy" : function(){
+      $scope.calc.state = "standBy";
+    },
+    "setResult" : function(){
+      $scope.calc.state = "result";
+    },
+    "standBy" : function(){
+      return "standBy";
+    },
+    "result" : function(){
+      return "result";
+    }
   };
 
-  $scope.doneCount = function() {
-  	var count = 0;
-    angular.forEach($scope.tasks, function(task){
-      count += task.done ? 1 : 0;
-    });
-  	return count;
-  };
+  // init
+  $scope.clearAll();
 
-  $scope.deleteDone = function() {
-    var oldTasks = $scope.tasks;
-    $scope.tasks = [];
-    angular.forEach(oldTasks, function(task){
-      if (task.done === false) {
-        $scope.tasks.push(task);
-      }
-    });
-  };
+  // defining functions
+  function _go(path){
+    $location.path(path);
+  }
+
+  function _clearAll(){
+    $scope.calc = {
+      "state"  : "standBy",
+      "sign"   : "",
+      "display": '0',
+      "stuck"  : 0
+    };
+    console.log("clearAll");
+  }
+
+  function _addNum(num){
+    if ($scope.calc.state === state.result() ) {
+      $scope.calc.display = num;
+      state.setStandBy();
+    }
+    if ($scope.calc.display !== '0') {
+      $scope.calc.display += num;
+    }else{
+      $scope.calc.display = num;
+    }
+    console.log("addNum: ", num);
+  }
+
+  function _addSign(sign){
+    $scope.calc.sign = sign;
+    if ($scope.calc.stuck === 0) {
+      $scope.calc.stuck = +$scope.calc.display;
+      $scope.calc.display = '0';
+    }
+  }
+
+  function _equal(){
+    if (!!!$scope.calc.sign) {
+      return;
+    }
+
+    var res = result();
+    _clearAll();
+    $scope.calc.display = res + '';
+    state.setResult();
+    console.log('state: ', $scope.calc.state);
+  }
+
+  function _addPoint(){
+    if ( !~$scope.calc.display.indexOf('.')) {
+      $scope.calc.display += '.';
+      console.log("addPoint");
+    }else{
+      console.log("ignore: addPoint");
+    }
+  }
+
+  function result(){
+    var res;
+    switch($scope.calc.sign){
+      case '+':
+        res = $scope.calc.stuck + Number($scope.calc.display);
+        break;
+      case '-':
+        res = $scope.calc.stuck - +$scope.calc.display;
+        break;
+      case '×':
+        res = $scope.calc.stuck * +$scope.calc.display;
+        break;
+      case '÷':
+        res = $scope.calc.stuck / +$scope.calc.display;
+        break;
+    }
+    return res;
+  }
+
+
 }]);
